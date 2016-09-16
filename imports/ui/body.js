7d8fdda17d5b9ae2387c3e920cc5034e6f7729ef
Vinicius Camargo <cmrg.vinicius@gmail.com>
2016-09-16 19:03:33 -0300

    Forms and events

--- imports/ui/body.js
+++ imports/ui/body.js
@@ -6,9 +6,25 @@ import './body.html';
 
 Template.body.helpers({
   tasks() {
-    return Tasks.find({});
+    return Tasks.find({}, { sort: { createdAt: -1 } });
   },
   // tasks: [
   //   { text: 'vinicius' }
   // ]
 });
+
+Template.body.events({
+  'submit .new-task'(event) {
+    event.preventDefault();
+
+    const target = event.target;
+    const text = target.text.value;
+
+    Tasks.insert({
+      text,
+      createdAt: new Date()
+    });
+
+    target.text.value = '';
+  }
+})
