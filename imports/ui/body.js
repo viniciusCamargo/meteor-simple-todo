0234c79884b7f86c071f9311da1096c47565c979
Vinicius Camargo <cmrg.vinicius@gmail.com>
2016-09-16 23:54:42 -0300

    Temporary UI state

--- imports/ui/body.js
+++ imports/ui/body.js
@@ -1,17 +1,30 @@
 import { Template } from 'meteor/templating';
+import { ReactiveDict } from 'meteor/reactive-dict';
 
 import { Tasks } from '../api/tasks.js';
 
 import './task.js';
 import './body.html';
 
+Template.body.onCreated(function bodyOnCreated() {
+  this.state = new ReactiveDict();
+});
+
 Template.body.helpers({
   tasks() {
+    const instance = Template.instance();
+    if (instance.state.get('hideCompleted')) {
+      return Tasks.find(
+        { checked: { $ne: true } }, // $ne = not equal to
+        { sort: { createdAt: -1 } }
+      );
+    }
+
     return Tasks.find({}, { sort: { createdAt: -1 } });
   },
-  // tasks: [
-  //   { text: 'vinicius' }
-  // ]
+  incompleteCount() {
+    return Tasks.find({ checked: { $ne: true } }).count();
+  }
 });
 
 Template.body.events({
@@ -28,5 +41,8 @@ Template.body.events({
     });
 
     target.text.value = '';
+  },
+  'change .hide-completed input'(event, instance) {
+    instance.state.set('hideCompleted', event.target.checked);
   }
 })
