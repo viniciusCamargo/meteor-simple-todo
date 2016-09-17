f4707b6c525aa3a77a54f2b0372a956d3fed06c2
Vinicius Camargo <cmrg.vinicius@gmail.com>
2016-09-17 00:42:12 -0300

    Adding user accounts

--- imports/ui/body.js
+++ imports/ui/body.js
@@ -1,3 +1,4 @@
+import { Meteor } from 'meteor/meteor';
 import { Template } from 'meteor/templating';
 import { ReactiveDict } from 'meteor/reactive-dict';
 
@@ -37,6 +38,8 @@ Template.body.events({
     Tasks.insert({
       text,
       createdAt: new Date(),
+      owner: Meteor.userId(),
+      username: Meteor.user().username,
       checked: false
     });
 
