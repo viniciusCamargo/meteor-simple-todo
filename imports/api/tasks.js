b7665c7c979e238918ec116ea0046f389e1522af
Vinicius Camargo <cmrg.vinicius@gmail.com>
2016-09-17 03:06:54 -0300

    Publish and subscribe

--- imports/api/tasks.js
+++ imports/api/tasks.js
@@ -4,6 +4,17 @@ import { check } from 'meteor/check';
 
 export const Tasks = new Mongo.Collection('tasks');
 
+if (Meteor.isServer) {
+  Meteor.publish('tasks', function tasksPublication() {
+    return Tasks.find({
+      $or: [
+        { private: { $ne: true } },
+        { owner: this.userId }
+      ]
+    });
+  });
+}
+
 Meteor.methods({
   'tasks.insert'(text) {
     check(text, String);
@@ -16,11 +27,18 @@ Meteor.methods({
       text,
       createdAt: new Date(),
       owner: this.userId,
-      username: Meteor.users.findOne(this.userId).username
+      username: Meteor.users.findOne(this.userId).username,
+      private: true
     });
   },
   'tasks.remove'(taskId) {
     check(taskId, String);
+
+    const task = Tasks.findOne(taskId);
+    // if (task.private && task.owner !== this.userId) {
+    if (task.owner !== this.userId) {
+      throw new Meteor.Error('not-authorized');
+    }
 
     Tasks.remove(taskId);
   },
@@ -28,6 +46,24 @@ Meteor.methods({
     check(taskId, String);
     check(setChecked, Boolean);
 
+    const task = Tasks.findOne(taskId);
+    // if (task.private && task.owner !== this.userId) {
+    if (task.owner !== this.userId) {
+      throw new Meteor.Error('not-authorized');
+    }
+
     Tasks.update(taskId, { $set: { checked: setChecked } });
+  },
+  'tasks.setPrivate'(taskId, setToPrivate) {
+    check(taskId, String),
+    check(setToPrivate, Boolean);
+
+    const task = Tasks.findOne(taskId);
+
+    if (task.owner !== this.userId) {
+      throw new Meteor.Error('not-authorized');
+    }
+
+    Tasks.update(taskId, { $set: { private: setToPrivate } });
   }
 });
--- imports/ui/task.js
+++ imports/ui/task.js
@@ -3,6 +3,12 @@ import { Template } from 'meteor/templating';
 import { Tasks } from '../api/tasks.js';
 import './task.html';
 
+Template.task.helpers({
+  isOwner() {
+    return this.owner === Meteor.userId();
+  }
+});
+
 Template.task.events({
   'click .toggle-checked'() {
     // Tasks.update(this._id, {
@@ -13,5 +19,8 @@ Template.task.events({
   'click .delete'() {
     // Tasks.remove(this._id)
     Meteor.call('tasks.remove', this._id);
+  },
+  'click .toggle-private'() {
+    Meteor.call('tasks.setPrivate', this._id, !this.private);
   }
-})
+});
